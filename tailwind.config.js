ac747a945b6c84c3d0ad17ef342f3d9b4e573da8
kalong-leung <leonellus0123@gmail.com>
2023-11-08 14:22:12 -0500

    git init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}', 
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/lib/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('prettier-plugin-tailwindcss'),
+    require('@tailwindcss/typography'),
+  ],
+}
+
